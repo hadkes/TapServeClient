5c2be162b9971558bc285049e9a622e6b47da996
ahmadfa <ahmadfa@tcs.ie>
2017-02-14 14:23:23 +0000

    initial version

---
+++ src/main/webapp/js/login.js
@@ -0,0 +1,8 @@
+function signUp(){
+	var username = $('#username').val();
+	var password = $('#password').val();
+	var usertype = $('#usertype').val();
+	alert(username);
+	alert(password);
+	alert(usertype);
+}
