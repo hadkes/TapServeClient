593fde56866862cc00af75976f9f3d9bc72b2bab
ahmadfa <ahmadfa@tcs.ie>
2017-02-14 19:31:54 +0000

    changes for page tittle

--- src/main/webapp/js/login.js
+++ src/main/webapp/js/login.js
@@ -12,14 +12,16 @@ function signUp(){
 		url: 'http://localhost:8083/tapserve/register',
 		data: jsonString,
 		success: function(result){
-			alert(result);
 			if(usertype == "user"){
+				$(document).attr("title", "User Registration");
 				$('#pageContent').load( "pages/UserRegistrationPage.html" );
 			}
 			else if(usertype == "service_provider"){
+				$(document).attr("title", "Service Provider Registration");
 				$('#pageContent').load( "pages/SPRegistrationPage.html" );
 			}
 			else if(usertype == "administrator"){
+				$(document).attr("title", "Admin Registration");
 				$('#pageContent').load( "pages/AdminRegistrationPage.html" );
 			}
 		}
